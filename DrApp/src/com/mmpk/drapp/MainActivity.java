6580eeb8ee83acc2d1c2f1df58365543ad7b0fb0
Michal <m.flendrich@gmail.com>
2012-11-03 15:18:04 +0100

    mainactivity events created

--- DrApp/src/com/mmpk/drapp/MainActivity.java
+++ DrApp/src/com/mmpk/drapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.mmpk.drapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -16,5 +18,13 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void iNeedAdviceClicked(View v) {
+    	Log.i("mainActivity", "i need advice clicked");
+    }
+    
+    public void emergencyClicked(View v) {
+    	Log.i("mainActivity", "emergency clicked");
     }
 }
